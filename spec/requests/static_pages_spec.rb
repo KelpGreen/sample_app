e348e7e1f03c8eb36aa2a3b635bdf7b31bcc5b24
Kelp Green <gerard.labelle+github@gmail.com>
2013-02-13 18:22:56 -0800

    Removed "| Home" suffix from Home page window title.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,23 @@ require 'spec_helper'
 describe "Static pages" do
   
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+  let(:url_root) { '/static_pages'}
   
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit "#{url_root}/home"
       page.should have_selector('h1', :text => "Sample App")
     end
     
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+    it "should have the base title" do
+      visit "#{url_root}/home"
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit "#{url_root}/home"
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
